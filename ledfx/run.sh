d213e97c46878ef70e26bd5e2eb6a1a78b84d804
Imran Khan <encrustace@gmail.com>
2026-02-27 16:31:50 +0530

    Ingress

--- ledfx/run.sh
+++ ledfx/run.sh
@@ -1,19 +1,21 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/env bash
 set -e
 
 ARGS=""
 
-if [ "$(jq -r '.offline' /data/options.json)" = "true" ]; then
-  ARGS="$ARGS --offline"
-fi
+if [ -f /data/options.json ]; then
+  if [ "$(jq -r '.offline' /data/options.json)" = "true" ]; then
+    ARGS="$ARGS --offline"
+  fi
 
-if [ "$(jq -r '.clear_effects' /data/options.json)" = "true" ]; then
-  ARGS="$ARGS --clear-effects"
+  if [ "$(jq -r '.clear_effects' /data/options.json)" = "true" ]; then
+    ARGS="$ARGS --clear-effects"
+  fi
 fi
 
 echo "Starting LedFx with args: $ARGS"
 
-exec /usr/bin/ledfx \
+exec ledfx \
   --host 0.0.0.0 \
   --port 8888 \
   --config /data \
